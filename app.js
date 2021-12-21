480cbdf17f69149379c66a4685ba594e6a246ad0
michelbeyrouty <michelbeyrouty@gmail.com>
2021-12-21 17:29:35 +0200

    Refactor code

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ require('dotenv/config');
 
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Import Routes
-const postsRoute = require('./routes/posts');
-const userRoute = require('./routes/user');
+const expensesRoute = require('./routes/expenses');
 
-app.use('/posts', postsRoute);
-app.use('/user', userRoute);
+app.use('/expenses', expensesRoute);
 
 //Routes
 app.get('/', (req, res) => {
-	res.send('We are on HomePage')
+  res.send('We are on HomePage');
 });
 
 // Connect MongoDB
-mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true  }, () => {
-	console.log("Connected to DB")
+mongoose.connect(process.env.DB_CONNECTION, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}, () => {
+  console.log('Connected to DB');
 });
 
 app.listen(3000, () => {
-	console.log("Running RESTful API on port http://localhost:3000");
+  console.log('Running RESTful API on port http://localhost:3000');
 });
