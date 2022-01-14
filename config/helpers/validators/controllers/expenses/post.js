c073f27d37ed42ea20aae1f8d4141561a0e13e74
michelbeyrouty <michelbeyrouty@gmail.com>
2022-01-15 01:20:56 +0200

    Refactor post validator

--- config/helpers/validators/controllers/expenses/post.js
+++ config/helpers/validators/controllers/expenses/post.js
@@ -1,9 +1,24 @@
+const Joi = require('joi');
+const EXPENSE_TYPES = [
+  'Entertainment',
+  'Food',
+  'Bills',
+  'Transport',
+  'Other',
+];
 const { validationError } = require('../../../../errors');
-const expenseValidation = require('../../models/expense');
 
 module.exports = function validatePayload(data){
 
-  const error = expenseValidation(data);
+  const schema = Joi.object({
+    description: Joi.string(),
+    type: Joi.string(),
+    // type: Joi.string().in(EXPENSE_TYPES)
+    //   .required(),
+    value: Joi.number().required(),
+  });
+
+  const { error } = schema.validate(expense);
 
   if(error){
     throw new validationError(error.details[0].message);
