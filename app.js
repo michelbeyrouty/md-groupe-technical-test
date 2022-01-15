34342bffb553024a3b28ad459d96fb481b710617
michelbeyrouty <michelbeyrouty@gmail.com>
2022-01-15 12:58:39 +0200

    Refactor code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const routes = require('./config/routes');
+const routes = require('./api/routes');
 require('dotenv').config();
 
 const app = express();
--- config/routes/expenses.js
+++ config/routes/expenses.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const expenseControllers = require('../controllers').expenses;
+const expenseControllers = require('../../api/controllers').expenses;
 
 router.post('/' , expenseControllers.post);
 
