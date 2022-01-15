4f64ce4ae0a7cf10eb43ee8e2e8dc61b68545dbd
michelbeyrouty <michelbeyrouty@gmail.com>
2022-01-15 13:03:03 +0200

    Refactor code

--- api/index.js
+++ api/index.js
@@ -4,15 +4,15 @@ module.exports = {
   helpers: {
     models: {
       expenses: {
-        create: require('./models/expenses/create')(Expense),
-        get: require('./models/expenses/get')(Expense),
+        create: require('./helpers/models/expenses/create')(Expense),
+        get: require('./helpers/models/expenses/get')(Expense),
       },
     },
-  },
-  validators: {
-    expenses:{
-      post: require('./validators/expenses/post'),
-      get: require('./validators/expenses/get'),
+    validators: {
+      expenses:{
+        post: require('./helpers/validators/expenses/post'),
+        get: require('./helpers/validators/expenses/get'),
+      },
     },
   },
 };
