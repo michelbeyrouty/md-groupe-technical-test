b9ebc9054879d9217261a3371cc90cd2d45dc05c
michelbeyrouty <michelbeyrouty@gmail.com>
2022-01-15 13:48:39 +0200

    Refactor code

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ function _connectMongo(){
     useNewUrlParser: true,
   };
 
+  mongoose.set('useFindAndModify', false);
   mongoose.connect(process.env.DB_CONNECTION, paramethers , () => {
     console.log('Connected to DB');
   });
